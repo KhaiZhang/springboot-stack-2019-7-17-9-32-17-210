d4d15396e8d87ef0df04fe3c72d6c5e986276c11
Khai <KhaiZhang@163.com>
2019-07-21 17:26:01 +0800

    [khai][feat: create Prosecutor with constraint]

---
+++ src/main/java/com/tw/apistackbase/model/Prosecutor.java
@@ -0,0 +1,38 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Prosecutor {
+    @Id
+    @GeneratedValue
+    @Column(length = 255)
+    private long id;
+
+    @Column(nullable = false,length = 255)
+    private String name;
+
+    public Prosecutor() {
+    }
+
+    public Prosecutor(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
